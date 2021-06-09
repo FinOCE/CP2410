37f14e48a8e73f125cc3911a8a46843f6c5895a8
Finley Sherwood <me@itsf.in>
2021-06-09 18:19:39 +1000

    Complete 2/3 questions of prac_06

---
+++ Week 7/prac_06.py
@@ -0,0 +1,88 @@
+# Question 1
+# a
+"""
+/users/rt/courses/
+"""
+
+# b
+"""
+cs016/
+homeworks/
+programs/
+cs252/
+projects/
+papers/
+"""
+
+# c
+"""
+9
+"""
+
+# d
+"""
+1
+"""
+
+# e
+"""
+grades
+programs/
+"""
+
+# f
+"""
+papers/
+buyhigh
+sellow
+demos/
+market
+"""
+
+# g
+"""
+1
+"""
+
+# h
+"""
+4
+"""
+
+# Question 2
+# a
+"""
+
+"""
+
+# b
+"""
+1
+2
+4
+5
+3
+6
+8
+9
+7
+10
+"""
+
+# c
+"""
+4
+5
+2
+8
+9
+6
+10
+7
+3
+"""
+
+# Question 3
+from linked_binary_tree import LinkedBinaryTree
+
+tree = LinkedBinaryTree()
